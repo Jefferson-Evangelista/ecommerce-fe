a7136cb181a013f78e8d37eb3a2b9481579ab424
evangelistaxiv <evangelistaxiv14@gmail.com>
2024-05-30 16:07:37 +0800

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,93 @@
 import React from "react";
 
 const Footer = () => {
-  const links = ["about", "skincare", "makeup", "supplements", "contact"];
+  const aboutLinks = [
+    "about",
+    "about aphrodite",
+    "careers",
+    "socialmedia",
+    "affiliates",
+    "supplychain",
+    "sitemap",
+    "globalsites",
+  ];
+  const skincareLinks = ["skincare", "makeup", "skincare", "fragrance"];
+  const makeupLinks = ["makeup", "base", "contour", "eyemakeup", "lips"];
+  const supplementLinks = [
+    "supplements",
+    "hair & nails",
+    "vitamins",
+    "nutrients",
+  ];
+  const luxuryLinks = ["luxury", "makeup", "skincare", "fragrance"];
+
   return (
-    <footer className="h-32 bg-dark px-8 py-4 text-light">
-      <div className="flex justify-between">
-        <a className="font-krona text-2xl text-yellow " href="">
-          Aphrodite
-        </a>
-        <div className="flex items-center">
-          {links.map((link, index) => (
-            <a className="ml-4" href="/" key={index}>
-              {link}
-            </a>
-          ))}
+    <footer className="bg-dark px-8 py-10 text-light">
+      <div className="flex justify-around">
+        <div>
+          <a className="font-krona text-2xl text-yellow " href="">
+            Aphrodite
+          </a>
+          <p className="text-xs text-darkGrey">Be beautiful</p>
+        </div>
+        <div className="flex w-1/2 justify-evenly">
+          <div>
+            {aboutLinks.map((link, index) => (
+              <a
+                className={`${index === 0 ? "text-md font-krona" : "text-sm font-normal hover:text-yellow"} ml-4 flex flex-col`}
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+          <div>
+            {skincareLinks.map((link, index) => (
+              <a
+                className={`${index === 0 ? "text-md font-krona" : "text-sm font-normal hover:text-yellow"} ml-4 flex flex-col`}
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+          <div>
+            {makeupLinks.map((link, index) => (
+              <a
+                className={`${index === 0 ? "text-md font-krona" : "text-sm font-normal hover:text-yellow"} ml-4 flex flex-col`}
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+          <div>
+            {supplementLinks.map((link, index) => (
+              <a
+                className={`${index === 0 ? "text-md font-krona" : "text-sm font-normal hover:text-yellow"} ml-4 flex flex-col`}
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+          <div>
+            {luxuryLinks.map((link, index) => (
+              <a
+                className={`${index === 0 ? "text-md font-krona" : "text-sm font-normal hover:text-yellow"} ml-4 flex flex-col`}
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
-
-      <p className="text-xs text-darkGrey">Be beautiful</p>
     </footer>
   );
 };
